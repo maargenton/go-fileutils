4f261b902110310ce02d2529af19b50af3469e76
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2021-09-12 16:05:03 -0700

    Re-enable tests

--- exist_test.go
+++ exist_test.go
@@ -1,24 +1,24 @@
 package fileutils_test
 
-// import (
-// 	"os"
-// 	"testing"
+import (
+	"os"
+	"testing"
 
-// 	"github.com/maargenton/go-testpredicate/pkg/verify"
+	"github.com/maargenton/go-testpredicate/pkg/verify"
 
-// 	"github.com/maargenton/go-fileutils"
-// )
+	"github.com/maargenton/go-fileutils"
+)
 
-// func TestExistsFunctionsWithFile(t *testing.T) {
-// 	filename := "exist_test.go"
-// 	verify.That(t, fileutils.Exists(filename)).IsTrue()
-// 	verify.That(t, fileutils.IsFile(filename)).IsTrue()
-// 	verify.That(t, fileutils.IsDir(filename)).IsFalse()
-// }
+func TestExistsFunctionsWithFile(t *testing.T) {
+	filename := "exist_test.go"
+	verify.That(t, fileutils.Exists(filename)).IsTrue()
+	verify.That(t, fileutils.IsFile(filename)).IsTrue()
+	verify.That(t, fileutils.IsDir(filename)).IsFalse()
+}
 
-// func TestExistsFunctionsWithDir(t *testing.T) {
-// 	path, _ := os.Getwd()
-// 	verify.That(t, fileutils.Exists(path)).IsTrue()
-// 	verify.That(t, fileutils.IsFile(path)).IsFalse()
-// 	verify.That(t, fileutils.IsDir(path)).IsTrue()
-// }
+func TestExistsFunctionsWithDir(t *testing.T) {
+	path, _ := os.Getwd()
+	verify.That(t, fileutils.Exists(path)).IsTrue()
+	verify.That(t, fileutils.IsFile(path)).IsFalse()
+	verify.That(t, fileutils.IsDir(path)).IsTrue()
+}
